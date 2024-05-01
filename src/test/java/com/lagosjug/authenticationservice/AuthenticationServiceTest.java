6f9526a5d8338f7630f1ba64cc79693e07d9e680
MayorJay <joseolu4gsm@yahoo.com>
2024-05-01 15:34:21 +0100

    feat: Setup standard code checks and GitHub actions

---
+++ src/test/java/com/lagosjug/authenticationservice/AuthenticationServiceTest.java
@@ -0,0 +1,12 @@
+package com.lagosjug.authenticationservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AuthenticationServiceTest {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertEquals(31, 73 - 42);
+    }
+}
